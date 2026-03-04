c9fcadf1f4946c3afee0c51e07140e889836ebf2
Eva Alonso <evam.alonso95@gmail.com>
2026-03-04 11:22:51 +0100

    feat: add custom fonts and update styles for typography

--- src/AnimalCrossingApp.tsx
+++ src/AnimalCrossingApp.tsx
@@ -2,8 +2,11 @@ import { Button } from "./components/ui/button";
 
 export const AnimalCrossingApp = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <h1 className="text-8xl font-acnh-title">Hola Mundo</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 };
