274fee17ec516a49ed394dba08aca923ccf35d48
Eva Alonso <evam.alonso95@gmail.com>
2026-03-04 11:45:57 +0100

    feat: restructure header and update background styling with gradient

--- src/AnimalCrossingApp.tsx
+++ src/AnimalCrossingApp.tsx
@@ -3,8 +3,10 @@ import { Button } from "./components/ui/button";
 export const AnimalCrossingApp = () => {
   return (
     <>
-      <h1 className="text-8xl font-acnh-title">Hola Mundo</h1>
       <div className="flex min-h-svh flex-col items-center justify-center">
+        <h1 className="text-8xl justify-items-center font-acnh-title">
+          Hola Mundo
+        </h1>
         <Button>Click me</Button>
       </div>
     </>
