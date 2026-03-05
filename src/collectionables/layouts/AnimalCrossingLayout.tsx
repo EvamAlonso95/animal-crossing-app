9bf935b98e294dba1a7a52b45f6bb047e5a57682
Eva Alonso <evam.alonso95@gmail.com>
2026-03-05 10:11:04 +0100

    feat: enhance layout and styling for ItemCard and AnimalCrossingLayout components

--- src/collectionables/layouts/AnimalCrossingLayout.tsx
+++ src/collectionables/layouts/AnimalCrossingLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export const AnimalCrossingLayout = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="text-8xl justify-items-center font-acnh-title">
+      <h1 className="text-8xl justify-items-center font-acnh-title mt-4 mb-4">
         Museum Tracker
       </h1>
       <Outlet />
--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -11,16 +11,20 @@ export const ItemCard = () => {
 
   return (
     <>
-      {fossils.map((fossil) => (
-        <div className="flex items-center justify-center cursor-pointer size-48 bg-indigo-300 text-white mb-1 rounded-3xl">
-          <img
-            className="w-3/4 h-3/4 object-contain bg-indigo-100 rounded-2xl p-4 hover:p-1 hover:transition-all "
-            src={fossil.image_url}
-            alt="fossil"
-          />
-          <p>{fossil.name}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+        {fossils.map((fossil) => (
+          <div className="flex flex-col items-center justify-center cursor-pointer size-48 bg-indigo-300 text-white mb-1 rounded-3xl">
+            <img
+              className="w-3/4 h-3/4 object-contain bg-indigo-100 rounded-2xl p-4 hover:p-1 hover:transition-all "
+              src={fossil.image_url}
+              alt="fossil"
+            />
+            <p className="mt-1 font-acnh-text font-thin text-slate-700 ">
+              {fossil.name.charAt(0).toUpperCase() + fossil.name.slice(1)}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
