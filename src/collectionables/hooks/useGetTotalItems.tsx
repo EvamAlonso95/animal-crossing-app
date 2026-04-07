10717263e30b04c3a1110b87c4860c60b8e0ca09
Eva Alonso <evam.alonso95@gmail.com>
2026-04-07 12:51:15 +0200

    feat: integrate musseum count into total items calculation in useGetTotalItems hook

--- src/collectionables/hooks/useGetTotalItems.tsx
+++ src/collectionables/hooks/useGetTotalItems.tsx
@@ -5,8 +5,10 @@ import { getAllBugsAction } from "../bugs/actions/get-all-bugs.action";
 
 import type { Category } from "../types/categories.interface";
 import { getAllMollusksAction } from "../mollusks/action/get-all-mollusks.action";
+import { useMuseum } from "../context/MusseumContext";
 
 export const useGetTotalItems = () => {
+  const { musseumCount } = useMuseum();
   const { data: fossils = [] } = useAllCollectionables(
     "fossils",
     getAllFossilAction,
@@ -28,6 +30,7 @@ export const useGetTotalItems = () => {
     fishes: fishes.length,
     bugs: bugs.length,
     sea: mollusks.length,
+    musseum: musseumCount,
   };
   return totalItemsMap;
 };
